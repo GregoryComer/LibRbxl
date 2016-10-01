cb97d5cd8328acc7bd5c9638b2c870b7ed62d9ba
Gregory Comer <me@gregorycomer.com>
2016-09-30 21:55:03 -0600

    Vector3 improvements & tests

---
+++ LibRbxl.Test/Vector3Tests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibRbxl.Instances;
+using NUnit.Framework;
+
+namespace LibRbxl.Test
+{
+    [TestFixture]
+    public class Vector3Tests
+    {
+        [Test]
+        public void MatrixMultiply_Identity()
+        {
+            var v = new Vector3(1, 2, 3);
+            var m = Matrix3.Identity;
+
+            var product = v*m;
+
+            Assert.AreEqual(v, product);
+        }
+
+        [Test]
+        public void MatrixMultiply()
+        {
+            var v = new Vector3(3, -2, 1);
+            var m = new Matrix3((float)-0.224845082, (float)-0.35017547, (float)0.909297407, (float)-0.0412378013, (float)0.935775876, (float)0.35017547, (float)-0.973521471, (float)0.0412378013, (float)-0.224845082);
+            var expected = new Vector3((float)0.935113072, (float)-1.64508963, (float)-3.22788525);
+
+            var transformed = v*m;
+
+            Assert.AreEqual(expected, transformed);
+        }
+
+        [Test]
+        public void MatrixMultiply2()
+        {
+            var v = new Vector3(5, 8, -6);
+            var m = new Matrix3((float)0.936293423, (float)-0.289629489, (float)0.198669329, (float)0.312991828, (float)0.944702566, (float)-0.0978434011, (float)-0.159345075, (float)0.153792009, (float)0.975170374);
+            var expected = new Vector3((float)1.17241514, (float)9.70963955, (float)-5.4174118);
+
+            var transformed = v * m;
+
+            Assert.AreEqual(expected, transformed);
+        }
+
+        [Test]
+        public void Add()
+        {
+            var v1 = new Vector3(1, 2, 3);
+            var v2 = new Vector3(10, 20, 30);
+            var expected = new Vector3(11, 22, 33);
+
+            var sum = v1 + v2;
+
+            Assert.AreEqual(expected, sum);
+        }
+
+        [Test]
+        public void Difference()
+        {
+            var v1 = new Vector3(10, 20, 30);
+            var v2 = new Vector3(1, 2, 3);
+            var expected = new Vector3(9, 18, 27);
+
+            var difference = v1 - v2;
+
+            Assert.AreEqual(expected, difference);
+        }
+
+        [Test]
+        public void Dot()
+        {
+            var v1 = new Vector3(1, 2, 3);
+            var v2 = new Vector3(10, 20, 30);
+            var expected = 140f;
+
+            var dot = v1.Dot(v2);
+
+            Assert.AreEqual(expected, dot);
+        }
+
+        [Test]
+        public void Dot_Static()
+        {
+            var v1 = new Vector3(1, 2, 3);
+            var v2 = new Vector3(10, 20, 30);
+            var expected = 140f;
+
+            var dot = Vector3.Dot(v1, v2);
+
+            Assert.AreEqual(expected, dot);
+        }
+
+        [Test]
+        public void Scale()
+        {
+            var v = new Vector3(1, 2, 3);
+            var s = 2.0f;
+            var expected = new Vector3(2, 4, 6);
+
+            var scaled = v*s;
+
+            Assert.AreEqual(expected, scaled);
+        }
+
+        [Test]
+        public void Scale_Reversed()
+        {
+            var v = new Vector3(1, 2, 3);
+            var s = 2.0f;
+            var expected = new Vector3(2, 4, 6);
+
+            var scaled = s*v;
+
+            Assert.AreEqual(expected, scaled);
+        }
+
+        [Test]
+        public void Divide()
+        {
+            var v = new Vector3(2, 8, 16);
+            var s = 2.0f;
+            var expected = new Vector3(1, 4, 8);
+
+            var result = v/s;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Divide_Reversed()
+        {
+            var v = new Vector3(2, 4, 8);
+            var s = 16.0f;
+            var expected = new Vector3(8, 4, 2);
+
+            var scaled = s/v;
+
+            Assert.AreEqual(expected, scaled);
+        }
+    }
+}
--- LibRbxl/Instances/Vector3.cs
+++ LibRbxl/Instances/Vector3.cs
@@ -2,8 +2,10 @@
 
 namespace LibRbxl.Instances
 {
-    public struct Vector3
+    public struct Vector3 : IEquatable<Vector3>
     {
+        private const float Epsilon = 0.000001f;
+
         public float X { get; }
         public float Y { get; }
         public float Z { get; }
@@ -50,9 +52,60 @@ namespace LibRbxl.Instances
             return new Vector3(v.X / divisor, v.Y / divisor, v.Z / divisor);
         }
 
+        public static Vector3 operator /(float divisor, Vector3 v)
+        {
+            return new Vector3(divisor / v.X, divisor / v.Y, divisor/ v.Z);
+        }
+
         public static float Dot(Vector3 a, Vector3 b)
         {
             return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        }
+
+        public static Vector3 operator *(Vector3 v, Matrix3 m)
+        {
+            return new Vector3(v.X * m.R00 + v.Y * m.R01 + v.Z * m.R02,
+                v.X * m.R10 + v.Y * m.R11 + v.Z * m.R12,
+                v.X * m.R20 + v.Y * m.R21 + v.Z * m.R22);
+        }
+
+        public static Vector3 operator *(Matrix3 m, Vector3 v)
+        {
+            return new Vector3(v.X * m.R00 + v.Y * m.R01 + v.Z * m.R02,
+                v.X * m.R10 + v.Y * m.R11 + v.Z * m.R12,
+                v.X * m.R20 + v.Y * m.R21 + v.Z * m.R22);
+        }
+
+        public bool Equals(Vector3 other)
+        {
+            return Math.Abs(X - other.X) < Epsilon && Math.Abs(Y - other.Y) < Epsilon && Math.Abs(Z - other.Z) < Epsilon;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Vector3 && Equals((Vector3) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = X.GetHashCode();
+                hashCode = (hashCode*397) ^ Y.GetHashCode();
+                hashCode = (hashCode*397) ^ Z.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Vector3 left, Vector3 right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector3 left, Vector3 right)
+        {
+            return !left.Equals(right);
         }
     }
 }
